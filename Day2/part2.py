0e4afabbef68781f55bbec2dfef86d2e819bddae
pradeepbur <pradeep.burugupalli@fox.com>
2024-12-02 19:39:42 -0600

    updated day2

---
+++ Day2/part2.py
@@ -0,0 +1,57 @@
+safe = 0
+# file_path = "part1-example.txt"
+file_path = "part1-input.txt"
+try:
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        file.close()
+        lenght = len(lines)
+        for report in range(len(lines)):
+            compare_list = lines[report].strip().split()
+
+            compare_list = [int(s) for s in compare_list]
+
+            for i in range(1, len(compare_list)):
+                if compare_list[-1] - compare_list[0] > 0:
+                    if (
+                        compare_list[i] - compare_list[i - 1] >= 1
+                        and compare_list[i] - compare_list[i - 1] <= 3
+                    ):
+                        if i == len(compare_list) - 1:
+                            safe += 1
+                            print(compare_list)
+                    else:
+                        if (
+                            compare_list[i] - compare_list[i - 2] >= 1
+                            and compare_list[i] - compare_list[i - 2] <= 3
+                        ):
+                            if i == len(compare_list) - 1:
+                                safe += 1
+                                print(compare_list)
+                        else:
+                            break
+                elif compare_list[-1] - compare_list[0] < 0:
+                    if (
+                        compare_list[i] - compare_list[i - 1] <= -1
+                        and compare_list[i] - compare_list[i - 1] >= -3
+                    ):
+                        if i == len(compare_list) - 1:
+                            safe += 1
+                            print(compare_list)
+                    else:
+                        if (
+                            compare_list[i] - compare_list[i - 2] <= -1
+                            and compare_list[i] - compare_list[i - 2] >= -3
+                        ):
+                            if i == len(compare_list) - 1:
+                                safe += 1
+                                print(compare_list)
+                        else:
+                            break
+                else:
+                    break
+
+    print(f"The total safe reports are {safe}.")
+
+except FileNotFoundError:
+    print(f"The file was not found at {file_path}.")
